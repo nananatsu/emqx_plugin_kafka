096c7208c751adc523d8ff67068b14bd20a87002
nananatsu <sevennatsu@gmail.com>
2025-07-08 16:59:53 +0800

    修改hook方式使能处理多条规则

--- src/emqx_plugin_kafka_hook.erl
+++ src/emqx_plugin_kafka_hook.erl
@@ -63,72 +63,81 @@
 %% Called when the plugin application start
 load(ClientConf, ProducerConf, HooKConf) ->
     ClientId = start_kafka(ClientConf),
-    HookList = parse_hook(HooKConf),
+    GroupedHooks = parse_hook(HooKConf),
 
-    NProducers = lists:foldl(
-        fun(
-            {Hook, Filter, Key, Value, Topic, Seq, TopicProducerConf},
-            Acc
-        ) ->
-            Name = list_to_atom(lists:concat([atom_to_list(Hook), "_", Seq])),
-
-            case start_producer(ClientId, Topic, Name, ProducerConf, TopicProducerConf) of
-                {ok, PayloadFormat, Sync, Timeout, Producers} ->
-                    load_(
-                        Hook,
-                        {Filter, Producers, Key, Value, PayloadFormat, Sync, Timeout}
-                    ),
-                    [Producers | Acc];
-                {error, _} ->
-                    Acc
+    {NProducers, ProcessedHooks} = lists:foldl(
+        fun({Hook, Rules}, {ProducerAcc, HookAcc}) ->
+            {RuleProducers, ProcessedRules} = lists:foldl(
+                fun({Filter, Key, Value, Topic, Seq, TopicProducerConf}, {PAcc, RAcc}) ->
+                    Name = list_to_atom(lists:concat([atom_to_list(Hook), "_", Seq])),
+                    case start_producer(ClientId, Topic, Name, ProducerConf, TopicProducerConf) of
+                        {ok, PayloadFormat, Sync, Timeout, Producers} ->
+                            Rule = {Filter, Producers, Key, Value, PayloadFormat, Sync, Timeout},
+                            {[Producers | PAcc], [Rule | RAcc]};
+                        {error, _} ->
+                            {PAcc, RAcc}
+                    end
+                end,
+                {[], []},
+                Rules
+            ),
+            %% 为这个hook点注册回调函数，传入所有规则
+            case ProcessedRules of
+                [] ->
+                    {ProducerAcc, HookAcc};
+                _ ->
+                    load_(Hook, ProcessedRules),
+                    {RuleProducers ++ ProducerAcc, [{Hook, ProcessedRules} | HookAcc]}
             end
         end,
-        [],
-        HookList
+        {[], []},
+        GroupedHooks
     ),
     logger:info("~s is loaded.~n", [emqx_plugin_kafka]),
-    {ok, ClientId, NProducers, HookList}.
+    {ok, ClientId, NProducers, ProcessedHooks}.
 
-load_(Hook, Params) ->
+load_(Hook, Rules) ->
     case Hook of
         'client.connect' ->
-            hook(Hook, {?MODULE, on_client_connect, [Params]});
+            hook(Hook, {?MODULE, on_client_connect, [Rules]});
         'client.connack' ->
-            hook(Hook, {?MODULE, on_client_connack, [Params]});
+            hook(Hook, {?MODULE, on_client_connack, [Rules]});
         'client.connected' ->
-            hook(Hook, {?MODULE, on_client_connected, [Params]});
+            hook(Hook, {?MODULE, on_client_connected, [Rules]});
         'client.disconnected' ->
-            hook(Hook, {?MODULE, on_client_disconnected, [Params]});
+            hook(Hook, {?MODULE, on_client_disconnected, [Rules]});
         'client.authenticate' ->
-            hook(Hook, {?MODULE, on_client_authenticate, [Params]});
+            hook(Hook, {?MODULE, on_client_authenticate, [Rules]});
         'client.authorize' ->
-            hook(Hook, {?MODULE, on_client_authorize, [Params]});
+            hook(Hook, {?MODULE, on_client_authorize, [Rules]});
         'client.subscribe' ->
-            hook(Hook, {?MODULE, on_client_subscribe, [Params]});
+            hook(Hook, {?MODULE, on_client_subscribe, [Rules]});
         'client.unsubscribe' ->
-            hook(Hook, {?MODULE, on_client_unsubscribe, [Params]});
+            hook(Hook, {?MODULE, on_client_unsubscribe, [Rules]});
         'session.created' ->
-            hook(Hook, {?MODULE, on_session_created, [Params]});
+            hook(Hook, {?MODULE, on_session_created, [Rules]});
         'session.subscribed' ->
-            hook(Hook, {?MODULE, on_session_subscribed, [Params]});
+            hook(Hook, {?MODULE, on_session_subscribed, [Rules]});
         'session.unsubscribed' ->
-            hook(Hook, {?MODULE, on_session_unsubscribed, [Params]});
+            hook(Hook, {?MODULE, on_session_unsubscribed, [Rules]});
         'session.resumed' ->
-            hook(Hook, {?MODULE, on_session_resumed, [Params]});
+            hook(Hook, {?MODULE, on_session_resumed, [Rules]});
         'session.discarded' ->
-            hook(Hook, {?MODULE, on_session_discarded, [Params]});
+            hook(Hook, {?MODULE, on_session_discarded, [Rules]});
         'session.takenover' ->
-            hook(Hook, {?MODULE, on_session_takenover, [Params]});
+            hook(Hook, {?MODULE, on_session_takenover, [Rules]});
         'session.terminated' ->
-            hook(Hook, {?MODULE, on_session_terminated, [Params]});
+            hook(Hook, {?MODULE, on_session_terminated, [Rules]});
         'message.publish' ->
-            hook(Hook, {?MODULE, on_message_publish, [Params]});
+            hook(Hook, {?MODULE, on_message_publish, [Rules]});
         'message.delivered' ->
-            hook(Hook, {?MODULE, on_message_delivered, [Params]});
+            hook(Hook, {?MODULE, on_message_delivered, [Rules]});
         'message.acked' ->
-            hook(Hook, {?MODULE, on_message_acked, [Params]});
+            hook(Hook, {?MODULE, on_message_acked, [Rules]});
         'message.dropped' ->
-            hook(Hook, {?MODULE, on_message_dropped, [Params]})
+            hook(Hook, {?MODULE, on_message_dropped, [Rules]});
+        _ ->
+            logger:warning("Unknown hook: ~p", [Hook])
     end.
 
 unload(HookList) ->
@@ -191,87 +200,101 @@ hook(HookPoint, MFA) ->
 unhook(HookPoint, MFA) ->
     emqx_hooks:del(HookPoint, MFA).
 
-on_client_connect(ClientInfo, Props, Rule) ->
-    send(ClientInfo, Rule),
+on_client_connect(ClientInfo, Props, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, Props}.
 
-on_client_connack(ClientInfo, _Rc, Props, Rule) ->
-    send(ClientInfo, Rule),
+on_client_connack(ClientInfo, _Rc, Props, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, Props}.
 
-on_client_connected(ClientInfo, _ConnInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_client_connected(ClientInfo, _ConnInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_client_disconnected(ClientInfo, _Reason, _ConnInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_client_disconnected(ClientInfo, _Reason, _ConnInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_client_authenticate(ClientInfo, AuthNResult, Rule) ->
-    send(ClientInfo, Rule),
+on_client_authenticate(ClientInfo, AuthNResult, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, AuthNResult}.
 
-on_client_authorize(ClientInfo, _PubSub, _Topic, AuthNResult, Rule) ->
-    send(ClientInfo, Rule),
+on_client_authorize(ClientInfo, _PubSub, _Topic, AuthNResult, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, AuthNResult}.
 
-on_client_subscribe(ClientInfo, _Props, TopicFilters, Rule) ->
-    send(ClientInfo, Rule),
+on_client_subscribe(ClientInfo, _Props, TopicFilters, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, TopicFilters}.
 
-on_client_unsubscribe(ClientInfo, _Props, TopicFilters, Rule) ->
-    send(ClientInfo, Rule),
+on_client_unsubscribe(ClientInfo, _Props, TopicFilters, Rules) ->
+    send_all(ClientInfo, Rules),
     {ok, TopicFilters}.
 
-on_session_created(ClientInfo, _SessInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_session_created(ClientInfo, _SessInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_session_subscribed(ClientInfo, Topic, Opts, Rule) ->
-    send(ClientInfo, Topic, maps:get(qos, Opts, 0), Rule),
+on_session_subscribed(ClientInfo, Topic, Opts, Rules) ->
+    send_all(ClientInfo, Topic, maps:get(qos, Opts, 0), Rules),
     ok.
 
-on_session_unsubscribed(ClientInfo, Topic, Opts, Rule) ->
-    send(ClientInfo, Topic, maps:get(qos, Opts, 0), Rule),
+on_session_unsubscribed(ClientInfo, Topic, Opts, Rules) ->
+    send_all(ClientInfo, Topic, maps:get(qos, Opts, 0), Rules),
     ok.
 
-on_session_resumed(ClientInfo, _SessInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_session_resumed(ClientInfo, _SessInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_session_discarded(ClientInfo, _SessInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_session_discarded(ClientInfo, _SessInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_session_takenover(ClientInfo, _SessInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_session_takenover(ClientInfo, _SessInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_session_terminated(ClientInfo, _Reason, _SessInfo, Rule) ->
-    send(ClientInfo, Rule),
+on_session_terminated(ClientInfo, _Reason, _SessInfo, Rules) ->
+    send_all(ClientInfo, Rules),
     ok.
 
-on_message_publish(Msg = #message{topic = <<"$SYS/", _/binary>>}, _Rule) ->
-    {ok, Msg};
-on_message_publish(Msg, Rule) ->
-    send(Msg, Rule),
+on_message_publish(Msg, _Rules) when is_record(Msg, message) ->
+    case emqx_message:topic(Msg) of
+        <<"$SYS/", _/binary>> ->
+            {ok, Msg};
+        _ ->
+            send_all(Msg, _Rules),
+            {ok, Msg}
+    end;
+on_message_publish(Msg, Rules) ->
+    send_all(Msg, Rules),
     {ok, Msg}.
 
-on_message_delivered(_ClientInfo, Msg, Rule) ->
-    send(Msg, Rule),
+on_message_delivered(_ClientInfo, Msg, Rules) ->
+    send_all(Msg, Rules),
     {ok, Msg}.
 
-on_message_acked(_ClientInfo, Msg, Rule) ->
-    send(Msg, Rule),
+on_message_acked(_ClientInfo, Msg, Rules) ->
+    send_all(Msg, Rules),
     ok.
 
-on_message_dropped(#message{topic = <<"$SYS/", _/binary>>}, _By, _Reason, _Rule) ->
-    ok;
-on_message_dropped(Msg, _By, _Reson, Rule) ->
-    send(Msg, Rule),
+on_message_dropped(Msg, _By, _Reason, _Rules) when is_record(Msg, message) ->
+    case emqx_message:topic(Msg) of
+        <<"$SYS/", _/binary>> ->
+            ok;
+        _ ->
+            send_all(Msg, _Rules),
+            ok
+    end;
+on_message_dropped(Msg, _By, _Reson, Rules) ->
+    send_all(Msg, Rules),
     ok.
 
-parse_hook(Hooks) -> parse_hook(Hooks, [], 0).
+parse_hook(Hooks) ->
+    HookList = parse_hook(Hooks, [], 0),
+    group_hooks_by_point(HookList).
 
 parse_hook([], Acc, _Seq) ->
     Acc;
@@ -301,11 +324,47 @@ parse_hook([Item | Hooks], Acc, Seq) ->
         NewSeq
     ).
 
+%% 将同一hook点的规则分组
+group_hooks_by_point(HookList) ->
+    HookMap = lists:foldl(
+        fun({Hook, Filter, Key, Value, Topic, Seq, ProducerConf}, Acc) ->
+            Rule = {Filter, Key, Value, Topic, Seq, ProducerConf},
+            case maps:get(Hook, Acc, undefined) of
+                undefined ->
+                    maps:put(Hook, [Rule], Acc);
+                ExistingRules ->
+                    maps:put(Hook, [Rule | ExistingRules], Acc)
+            end
+        end,
+        #{},
+        HookList
+    ),
+    maps:to_list(HookMap).
+
 format_value_pattern(undefined) ->
     [];
 format_value_pattern(Value) ->
     emqx_placeholder:preproc_tmpl(Value).
 
+%% 发送消息到所有匹配的规则
+send_all(Data, Rules) ->
+    lists:foreach(
+        fun(Rule) ->
+            send(Data, Rule)
+        end,
+        Rules
+    ).
+
+%% 带Topic和Qos的发送函数
+send_all(ClientInfo, Topic, Qos, Rules) ->
+    lists:foreach(
+        fun(Rule) ->
+            send(ClientInfo, Topic, Qos, Rule)
+        end,
+        Rules
+    ).
+
+%% 单个规则的发送函数 - 处理ClientInfo
 send(
     #{clientid := ClientId, username := Username},
     {_, Producers, Key, Value, _, Sync, Timeout}
@@ -326,19 +385,16 @@ send(
         Sync,
         Timeout
     );
-send(
-    #message{
-        from = From,
-        topic = Topic,
-        payload = Payload,
-        headers = Headers,
-        qos = Qos,
-        timestamp = Ts
-    },
-    {Filter, Producers, Key, Value, PayloadFormat, Sync, Timeout}
-) ->
+%% 单个规则的发送函数 - 处理Message
+send(Msg, {Filter, Producers, Key, Value, PayloadFormat, Sync, Timeout}) when is_record(Msg, message) ->
+    Topic = emqx_message:topic(Msg),
     case emqx_topic:match(Topic, Filter) of
         true ->
+            From = emqx_message:from(Msg),
+            Payload = emqx_message:payload(Msg),
+            Headers = emqx_message:headers(Msg),
+            Qos = emqx_message:qos(Msg),
+            Ts = emqx_message:timestamp(Msg),
             send_msg_to_kafka(
                 Producers,
                 {
@@ -359,6 +415,7 @@ send(
             ok
     end.
 
+%% 带Topic和Qos的单个规则发送函数
 send(ClientInfo, Topic, Qos, {Filter, Producers, Key, Value, _, Sync, Timeout}) ->
     case emqx_topic:match(Topic, Filter) of
         true ->
